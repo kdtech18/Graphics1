6f26c49205db1550c7c7d2f1193c58851cbb1db6
kdtech18 <kdtech18@gmail.com>
2016-11-15 10:21:40 -0500

    Initial commit

---
+++ src/GraphicWithName.java
@@ -0,0 +1,21 @@
+/* GraphicWithName.java
+   Kenton Duprey
+   Java Graphics II G
+   Mr. Blondin
+   11/15/2016
+*/
+
+import javax.swing.*;
+
+public class GraphicWithName
+	{
+	public static void main(String[] args)
+		{
+		
+		JFrame frame1 = new JFrame();
+		JPanel panel1 = new JPanel();
+		
+		
+		}// end main method
+	
+	}
