234d451f9dd811cd0f35a1e44e0ad5aeceb3ba07
kdtech18 <kdtech18@gmail.com>
2016-11-15 10:52:41 -0500

    added GUI x2 with label

--- src/GraphicWithName.java
+++ src/GraphicWithName.java
@@ -6,13 +6,17 @@
 */
 
 import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 
 public class GraphicWithName
 	{
 	public static void main(String[] args)
 		{
+		JLabel label = new JLabel("My label");
+		
+		label.setText("<html>This is a<br>multiline label!<br> Try it yourself!</html>");
 		JOptionPane.showMessageDialog(null, "Kenton Duprey","Name", JOptionPane.ERROR_MESSAGE);
-		JOptionPane.showMessageDialog(null, "My Name", "Title of Box", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, label, "Title of Box", JOptionPane.ERROR_MESSAGE);
 		
 		
 		}// end main method
